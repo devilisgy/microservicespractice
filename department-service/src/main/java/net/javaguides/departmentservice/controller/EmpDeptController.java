f041e4caeae933918f96aa1dcc76659422d0efaf
devilisgy <marcosdevil123@gmail.comgit config --global user.email marcosdevil123@gmail.com>
2024-11-17 09:04:03 +0530

    commit DepController1

--- department-service/src/main/java/net/javaguides/departmentservice/controller/EmpDeptController.java
+++ department-service/src/main/java/net/javaguides/departmentservice/controller/EmpDeptController.java
@@ -1,4 +1,23 @@
 package net.javaguides.departmentservice.controller;
 
+import net.javaguides.departmentservice.dto.DepartmentDto;
+import net.javaguides.departmentservice.service.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class EmpDeptController {
+    private DepartmentService departmentService;
+
+    // Build save department REST API
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        System.out.println(savedDepartment +"Hi");
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+
+    }
+
+
 }
